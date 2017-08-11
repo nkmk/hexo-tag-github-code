c155e5d430e8c27648666ddf17c2b57bbd43b5dc
nkmk <nkmk.on@gmail.com>
2017-08-11 13:35:50 +0900

    add setting for caption and line number

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var https = require('https')
 var path = require('path')
+var assign = require('lodash.assign');
 
 function get_code(url, callback){
     var data = ''
@@ -14,21 +15,75 @@ function get_code(url, callback){
     })
 }
 
+function str2obj(s){
+    l = s.slice(1,-1).split(',')
+    o = {}
+    l.forEach(function(element) {
+        var pair = element.trim().split(':')
+        var value = pair[1].trim().toLowerCase()
+        if(value == 'true'){
+            value = true
+        }else if(value == 'false'){
+            value = false
+        }
+        o[pair[0].trim()] = value
+    })
+    return o
+}
+
 function ghcode(args){
     var codeTag = hexo.extend.tag.env.extensions.code.fn
     var url = args[0]
-    var start = args[1]
+    var options = {}
+    var start, stop
+    if(typeof(args[1]) == 'string' && args[1].charAt(0) == '{'){
+        options = str2obj(args[1])
+    }else{
+        start = args[1]
+        stop = args[2]
+    }
+    if(typeof(args[3]) == 'string' && args[3].charAt(0) == '{'){
+        options = str2obj(args[3])
+    }
     if(start == undefined){
         start = 1
     }
-    var stop = args[2]
+
+    var cap_default = true
+    var re_default = true
+
+    if('github_code' in hexo.config){
+        if('cap' in hexo.config.github_code){
+            cap_default = hexo.config.github_code.cap
+        }
+        if('re' in hexo.config.github_code){
+            re_default = hexo.config.github_code.re
+        }
+    }
+
+    options = assign({
+        cap: cap_default,
+        re: re_default
+    }, options);
+
+    console.log(options)
+
     var raw_url = url.replace(/github.com/, 'raw.githubusercontent.com').replace(/blob\//, '')
     var ext = path.extname(raw_url).slice(1)
     var basename = path.basename(raw_url)
     return new Promise(function(resolve, reject){
         get_code(raw_url, function(data){
             var split_data = data.split(/\r\n|\r|\n/).slice(start - 1, stop).join('\n')
-            var result = codeTag([basename, 'lang:' + ext, url, 'first_line:' + start], split_data)
+            var arg
+            if(options['cap']){
+                arg = [basename, 'lang:' + ext, url]
+            }else{
+                arg = ['lang:' + ext]
+            }
+            if(options['re']){
+                arg.push('first_line:' + start)
+            }
+            result = codeTag(arg, split_data)
             resolve(result)
         })
     })
